0e73e96be08d922b9cc4984e28f84b4bd6bad468
Tamilnaveen48 <naveentamil66@gmail.com>
2024-11-22 12:03:08 +0530

    2

--- backup-script.sh
+++ backup-script.sh
@@ -2,5 +2,5 @@
 #!/bin/bash
 echo "Running backup task..."
 # Example command to back up data
-tar -czvf backup_$(date +%F).tar.gz C:\Users\sakth\Desktop\Naan Mudhavan Project\index.html
+tar -czvf backup_$(date +%F).tar.gz C:\Users\sakth\Desktop\Naan Mudhavan Project
 echo "Backup completed."
